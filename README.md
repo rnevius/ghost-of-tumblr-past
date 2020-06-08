9b05f6992ec2a64b6478382c013e5b2fedbd9f6a
Ryan Nevius <ryanevius@gmail.com>
2020-06-08 13:11:24 +0200

    Add working conversion script

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
+# Ghost of Tumblr Past (Tumblr → Ghost)
+
 A simple Node script to convert Tumblr XML exports into Ghost-compatible JSON.
+For now, this doesn't translate Tumblr media URLs...so media will still point to
+Tumblr hosted versions (not ideal for future-proofing).
+
+1. Install dependencies with `yarn install`
+2. Run the script, passing the path to your Tumblr _posts.xml_ file:
+
+    node index.js /path/to/posts/posts.xml
+
+3. _ghosted.json_ will be created in the directory where the script is executed.
--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const { readFileSync } = require('fs');
-const parseString = require('xml2js').parseStringPromise;
+const { readFileSync, writeFileSync } = require('fs');
+const xml2js = require('xml2js');
 
 const xmlFile = readFileSync(process.argv[2], 'utf8');
-parseString(xmlFile, { attrkey: 'meta' }).then(({ tumblr }) => {
-  const ghostData = tumblr.posts[0].post.map((post) => (
-    {
-      created_at: post.meta['unix-timestamp'],
-      html: post['regular-body'],
-      id: post.meta.id,
-      slug: post.meta.slug,
-      status: post.meta.state,
-      title: post['regular-title'],
-    }
-  ));
-  console.log(ghostData);
-})
-.catch(console.log);
+xml2js.parseStringPromise(xmlFile, { attrkey: 'meta' }).then(({ tumblr }) => {
+  const posts = tumblr.posts[0].post
+    .filter((post) => (
+      post.meta.state === 'published' && post['regular-title']
+    ))
+    .map((post) => (
+      {
+        id: post.meta.id,
+        mobiledoc: JSON.stringify({
+          version: '0.3.1',
+          markups: [],
+          atoms: [],
+          cards: [['html', { cardName: 'html', html: post['regular-body'][0] }]],
+          sections: [[10, 0]],
+        }),
+        published_at: Date.parse(post.meta.date),
+        slug: post.meta.slug,
+        status: post.meta.state,
+        title: post['regular-title'][0],
+      }
+    ));
+
+  const ghostData = {
+    data: { posts },
+    meta: { exported_on: Date.now(), version: '2.14.0' },
+  };
+  writeFileSync('./ghosted.json', JSON.stringify(ghostData));
+});
